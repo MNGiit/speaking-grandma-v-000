ef176fdfc88f65f93befa1f864069b698dda4be9
Mohammed Noori <kmanoorib@gmail.com>
2018-05-20 06:57:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(string)
   
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == "WHAT DID YOU EAT TODAY?"
+  elsif string == string.upcase
     "NO, NOT SINCE 1938!"
   elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
